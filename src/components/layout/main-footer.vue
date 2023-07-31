6a9731985b3dc6be4968a98f2318460329814ad7
wico <wico0628@gmail.com>
2023-07-31 16:12:15 +0800

    feat: update footer's content

--- src/components/layout/main-footer.vue
+++ src/components/layout/main-footer.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="main_footer">
-        客服專線 02-2655-1775 ｜cs@newebpay.com｜服務時間24小時
+        <div class="main_footer_left">© 2018 Neweb Technologies Co., Ltd.</div>
+        <div class="main_footer_right">客服專線 02-2655-1775｜cs@newebpay.com｜服務時間24小時</div>
     </div>
 </template>
 
@@ -8,4 +9,13 @@
 </script>
 
 <style scoped lang="scss">
+$text: #606266;
+
+.main_footer {
+    display: flex;
+    justify-content: space-between;
+
+    align-items: center;
+    color: $text;
+}
 </style>
