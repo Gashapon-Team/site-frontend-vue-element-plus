84a1816380aaf05e9a05819166851edebbfb142f
wico <wico0628@gmail.com>
2023-07-31 15:12:33 +0800

    feat: set 'about page' as index, separate 'demo page', remove home view

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,7 @@
 <template>
-  <el-container>
-    <el-aside>
-      <left-menu></left-menu>
-    </el-aside>
-    <el-container>
-      <el-header>
-        <topHead></topHead>
-      </el-header>
-      <el-main>
-        <component :is="nowPage"></component>
-      </el-main>
-    </el-container>
-  </el-container>
+  <RouterView />
 </template>
 
 <script setup>
-import {ref,shallowRef} from 'vue';
-import leftMenu from './components/layout/menu.vue'
-import topHead from './components/layout/header.vue';
-
-import KYC from './components/member/KYC.vue';
-
-let nowPage = shallowRef(KYC);
-
-
-//變更右側功能component
-const changePage = (pageName) => {
-  nowPage.value = markRaw(pageName)
-}
-
-
+import { RouterView } from 'vue-router'
 </script>
-
-<style>
-  *{
-    margin:0px;
-    padding:0px;
-  }
-   
-  .el-header{
-    padding:0px !important;
-  }
-
-  .el-main{
-    background-color:#eeeeee;
-    min-height: calc(100vh - 60px);
-  }
-</style>
---
+++ src/views/DemoView.vue
@@ -0,0 +1,61 @@
+<template>
+
+  <el-container>
+    <el-aside>
+      <el-scrollbar>
+        <main-menu></main-menu>
+      </el-scrollbar>
+    </el-aside>
+
+    <el-container>
+      <el-header>
+        <main-header></main-header>
+      </el-header>
+
+      <el-main>
+        <component :is="contentPage"></component>
+      </el-main>
+
+      <el-footer>
+        <main-footer></main-footer>
+      </el-footer>
+    </el-container>
+
+  </el-container>
+
+</template>
+
+<script setup>
+// import {ref,shallowRef} from 'vue';
+import { shallowRef } from 'vue';
+
+import MainMenu from '../components/layout/main-menu.vue';
+import MainHeader from '../components/layout/main-header.vue';
+import MainFooter from '../components/layout/main-footer.vue';
+
+import KYC from '../components/member/KYC.vue';
+
+let contentPage = shallowRef(KYC);
+
+//變更右側功能component
+// const changePage = (pageName) => {
+//   nowPage.value = markRaw(pageName)
+// }
+</script>
+
+
+<style>
+* {
+  margin: 0px;
+  padding: 0px;
+}
+
+.el-header {
+  padding: 0px !important;
+}
+
+.el-main {
+  background-color: #eeeeee;
+  min-height: calc(100vh - 60px);
+}
+</style>
