2b9f3004a203f1cd1db34c5f587fc0057408728a
wico <wico.wen@neweb.com.tw>
2023-07-31 11:42:47 +0800

    feat: change site title, tidy code

--- src/components/layout/menu.vue
+++ src/components/layout/menu.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="left_menu p-3">
-        <!--logo-->
+
         <div class="logo">
-            <h1>NewebPay藍新金流</h1>
+            <h1>LoremIps店家名稱</h1>
             <h3>會員平台管理</h3>
         </div>
+
         <el-menu 
             default-active="0-0"
             background-color="Transparent"   
@@ -21,109 +22,108 @@
                 </el-menu-item>
             </el-sub-menu>
       </el-menu>
+
     </div>
 </template>
-  
+
 <script setup>
-import {ref, reactive} from 'vue';
+// import {ref, reactive} from 'vue';
+import { reactive } from 'vue';
 
 const menus = reactive([
     {
-        mainTitle:'會員中心',
-        icon:'UserFilled',
-        item:[
+        mainTitle: '會員中心',
+        icon: 'UserFilled',
+        item: [
             {
-                title:'基本資料設定',
+                title: '基本資料設定',
             },
             {
-                title:'實名認證 (KYC)',
+                title: '實名認證 (KYC)',
             },
             {
-                title:'商店',
+                title: '商店',
             },
             {
-                title:'帳戶餘額/提領',
+                title: '帳戶餘額/提領',
             },
             {
-                title:'發票/對帳單',
+                title: '發票/對帳單',
             },
             {
-                title:'服務加值/帳單',
+                title: '服務加值/帳單',
             },
             {
-                title:'設定',
+                title: '設定',
             },
             {
-                title:'消費者專區',
+                title: '消費者專區',
             },
             {
-                title:'平台商專區',
+                title: '平台商專區',
             },
         ]
     },
     {
-        mainTitle:'幫助中心',
-        icon:'WarningFilled',
-        item:[
+        mainTitle: '幫助中心',
+        icon: 'WarningFilled',
+        item: [
             {
-                title:'問與答',
+                title: '問與答',
             }
         ]
     },
     {
-        mainTitle:'通知中心',
-        icon:'WarningFilled',
+        mainTitle: '通知中心',
+        icon: 'WarningFilled',
     },
     {
-        mainTitle:'電子發票',
-        icon:'WarningFilled',
+        mainTitle: '電子發票',
+        icon: 'WarningFilled',
     },
     {
-        mainTitle:'捐款',
-        icon:'WarningFilled',
+        mainTitle: '捐款',
+        icon: 'WarningFilled',
     },
     {
-        mainTitle:'物流',
-        icon:'WarningFilled',
+        mainTitle: '物流',
+        icon: 'WarningFilled',
     }
 ]);
-
 </script>
 
 <style scoped lang="scss">
-.left_menu{
+.left_menu {
     position: fixed;
-    min-height:100vh;
+    min-height: 100vh;
     background-image: linear-gradient(to top right, #07229a, #0064ff);
-    padding:30px;
-    color:#FFFFFF;
+    padding: 30px;
+    color: #FFFFFF;
 
-    .logo{
-        h1{
-            font-size:28px;
-            margin-bottom:30px;
+    .logo {
+        h1 {
+            font-size: 28px;
+            margin-bottom: 30px;
         }
 
-        h3{
-            margin-bottom:20px;
+        h3 {
+            margin-bottom: 20px;
         }
     }
 
-    .el-menu{
-        border-width:0px;
+    .el-menu {
+        border-width: 0px;
     }
 
-    :deep(.el-icon.el-sub-menu__icon-arrow svg){
-        width:2em;
-        height:2em;
+    :deep(.el-icon.el-sub-menu__icon-arrow svg) {
+        width: 2em;
+        height: 2em;
     }
 
-    .el-menu-item.is-active{
-        background-color: rgba(255,255,255,0.3);
+    .el-menu-item.is-active {
+        background-color: rgba(255, 255, 255, 0.3);
         border-radius: 5px;
         color: #FFFFFF;
     }
-    
 }
 </style>
-  
