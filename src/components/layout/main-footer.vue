b51fc0653cceebbd08843bee9f88db8faea5baa9
wico <wico0628@gmail.com>
2023-07-31 15:13:11 +0800

    feat: rename header, footer, menu

---
+++ src/components/layout/main-footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="main_footer">
+        客服專線 02-2655-1775 ｜cs@newebpay.com｜服務時間24小時
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped lang="scss">
+</style>
