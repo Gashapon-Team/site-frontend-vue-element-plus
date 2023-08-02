906e0b0b203bc0bd6b3ebc720bdad385a0cdd7ab
EZPAY_OA\shang.huang <shang.huang@ezpay.com.tw>
2023-08-02 16:27:02 +0800

    feature: homepage layout setting

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,13 +1,19 @@
 <template>
-  This is Homepage.
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px"><Sidemenu /></el-aside>
+        <el-row><Header /></el-row>
+        <el-row> Main </el-row>
+    </el-container>
+  </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import Sidemenu from '../components/menu/SideMenu.vue'
+import Header from '../components/layout/header.vue'
+import { defineComponent } from 'vue'
 
-}
+defineComponent({ Sidemenu, Header })
 </script>
-
 <style>
-
 </style>
