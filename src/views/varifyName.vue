37a4d7a9213fce411b8ba0d66d558261c473e2e8
EZPAY_OA\shang.huang <shang.huang@ezpay.com.tw>
2023-08-08 16:51:52 +0800

    feature: history

--- src/views/varifyName.vue
+++ src/views/varifyName.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="varify_name">
-    <div class="varify_status">
+    <div class="varify_section varify_status">
         <h3 class="varify_status__title">實名認證(KYC)</h3>
         <p class="varify_status__content">依據《第三方支付服務業防治洗錢及打擊資恐辦法》規定，藍新有義務進行客戶身分調查...完成實名認證，請放心，身分資料僅用於...</p>
         <div class="varify_status__show_status">
@@ -14,6 +14,15 @@
           </div>
         </div>
     </div>
+
+    <div class="varify_section varify_history">
+        <h3 class="title">修改歷程</h3>
+        <div class="line"></div>
+        <div class="content">
+          <div class="no_data_text">尚無資料</div>
+        </div>
+    </div>
+
   </div>
 </template>
 <script>
@@ -23,12 +32,16 @@ export default {
 </script>
 
 <style lang="scss" scope>
+$border_color: #f0f1f1;
 #varify_name {
-  padding: 40px;
-  margin: 40px;
-  border: 1px solid #f0f1f1;
-  border-radius: 6px;
-  box-shadow: 0px 0px 0 1px #f0f1f1;
+  padding: 16px;
+  .varify_section {
+    padding: 14px;
+    margin: 40px;
+    border: 1px solid $border_color;
+    border-radius: 6px;
+    box-shadow: 0px 0px 0 1px $border_color;
+  }
   .varify_status {
     &__title {
       margin-bottom: 12px;
@@ -56,6 +69,26 @@ export default {
       }
     }
   }
+  .varify_history {
+    .title {
+      font-size: 16px;
+      font-weight: 600;
+      margin-bottom:8px;
+    }
+    .line {
+      width: calc(100% + 30px);
+      transform: translateX(-16px);
+      height: 1px;
+      background: $border_color;
+    }
+    .content {
+      height: 40vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+
 }
 
 </style>
